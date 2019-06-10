3bc20e7c614bb87ad2d0a8896acfc22c3fa91fb8
kingtobebetter@163.com <kingtobebetter@163.com>
2019-06-10 23:12:35 +0800

    2
    1、完善用户角色与权限模块
    2、新增用户个人资料页，与编辑个人资料页功能
    3、修改首页，增加博客输入框

--- app/models.py
+++ app/models.py
@@ -1,20 +1,72 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin,AnonymousUserMixin
 from . import login_manager
 from  itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
-from . import db
+from flask import current_app,request
+from datetime import datetime
+import hashlib
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean,default=False,index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __init__(self,**kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    # 判断是否有某权限
+    def has_permisson(self,perm):
+        return (self.permissions & perm) == perm
+
+    # 添加权限
+    def add_permission(self,perm):
+        if not self.has_permisson(perm):
+            self.permissions += perm
+
+    # 移除权限
+    def remove_permission(self,perm):
+        if self.has_permisson(perm):
+            self.permissions -= perm
+
+    # 权限初始化
+    def reset_permissons(self):
+        self.permissions = 0
 
     def __repr__(self):
         return '<Role %r>' % self.name
 
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User':[Permission.FOLLOW,Permission.COMMENT,Permission.WRITE],
+            'Moderator':[Permission.FOLLOW,Permission.COMMENT,Permission.WRITE,Permission.MODERATE],
+            'Administrator':[Permission.FOLLOW,Permission.COMMENT,Permission.WRITE,Permission.MODERATE,Permission.ADMIN]
+        }
+        default_role = 'User'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name = r)
+            role.reset_permissons()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
+# 这个类列出了各权限代表的权限值
+class Permission:
+    FOLLOW = 1
+    COMMENT = 2
+    WRITE = 4
+    MODERATE = 8
+    ADMIN = 16
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -24,6 +76,25 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean,default=False)
+    posts = db.relationship('Post',backref='author',lazy='dynamic')
+
+    # 用户个人资料页显示内容字段
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(),default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(),default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
+
+    def __init__(self,**kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['MAIL_USERNAME']:
+                self.role = Role.query.filter_by(name='Administrator').first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = self.gravatar_hash()
 
     @property
     def password(self):
@@ -51,7 +122,6 @@ class User(UserMixin,db.Model):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'reset': self.id}).decode('utf-8')
 
-
     def confirm(self,token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
@@ -64,7 +134,7 @@ class User(UserMixin,db.Model):
         db.session.add(self)
         return True
 
-#修改密码
+    # 修改密码
     @staticmethod
     def reset_password(token, new_password):
         s = Serializer(current_app.config['SECRET_KEY'])
@@ -78,3 +148,43 @@ class User(UserMixin,db.Model):
         user.password = new_password
         db.session.add(user)
         return True
+
+    # 检查用户是否有指定的权限
+    def can(self,perm):
+        return self.role is not None and self.role.has_permisson(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+        db.session.commit()
+
+    def gravatar_hash(self):
+        return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+
+    def gravatar(self,size=100,default='identicon',rating='g'):
+        if request.is_secure:
+            url = 'http://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or self.gravatar_hash()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url,hash=hash,size=size,default=default,rating=rating)
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissons):
+        return False
+
+    def is_administrator(self):
+        return False
+
+login_manager.anonymous_user = AnonymousUser
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer,primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
+    author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
